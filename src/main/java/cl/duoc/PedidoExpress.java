20918e64563e5cc5f4c1f759c8211a49957de310
KatherineA <kath.avila@duocuc.cl>
2026-08-16 20:57:02 -0400

    Creación de la clase PedidoExpress.java

---
+++ src/main/java/cl/duoc/PedidoExpress.java
@@ -0,0 +1,90 @@
+package cl.duoc;
+
+/**
+ * Clase que representa los pedidos Express en el sistema SpeedFast
+ * @author KatherineAvila
+ */
+public class PedidoExpress extends Pedido {
+
+    private String tipoComercio;
+    private int tiempoLLegada;
+    private boolean disponibilidadInmediata;
+
+    public PedidoExpress(String idPedido, String direccionEntrega, String tipoPedido, String tipoComercio, int tiempoLLegada, boolean disponibilidadInmediata) {
+        super(idPedido, direccionEntrega, tipoPedido);
+        this.tipoComercio = tipoComercio;
+        this.disponibilidadInmediata =  disponibilidadInmediata;
+        this.tiempoLLegada = tiempoLLegada;
+    }
+
+    public String getTipoComercio() {
+        return tipoComercio;
+    }
+
+    public void setTipoComercio(String tipoComercio) {
+        this.tipoComercio = tipoComercio;
+    }
+
+    public int getTiempoLLegada() {
+        return tiempoLLegada;
+    }
+
+    public void setTiempoLLegada(int tiempoLLegada) {
+        this.tiempoLLegada = tiempoLLegada;
+    }
+
+    public boolean isDisponibilidadInmediata() {
+        return disponibilidadInmediata;
+    }
+
+    public void setDisponibilidadInmediata(boolean disponibilidadInmediata) {
+        this.disponibilidadInmediata = disponibilidadInmediata;
+    }
+
+    /**
+     * Método sobreescrito que muestra mensaje personalizado
+     */
+    @Override
+    public void asignarRepartidor() {
+        System.out.println("---Pedido Express ---");
+        System.out.println("Asignando Repartidor...");
+
+        }
+
+    /**
+     * Método sobrecargado, evalúa y asigna un repartidor especifico al pedido
+     * Válida las reglas de negocio establecidas (tiempo máximo de llegada según el tipo de comercio y la disponibilidad inmediata). si el repartidor
+     * cumple co los requisitos, confirma la asignación. De lo contrario, notifica el rechazo.
+     * @param nombreRepartidor el nombre del repartidor que se intenta asignar al pedido.
+     */
+    public void asignarRepartidor(String nombreRepartidor){
+
+        if (tipoComercio.equalsIgnoreCase("Farmacia") && (tiempoLLegada <= 15)) {
+            if (disponibilidadInmediata) {
+                System.out.println("Repartidor asignado " + nombreRepartidor);
+                System.out.println("Tu repartidor esta a: " + getTiempoLLegada() + " minutos.");
+            } else {
+                System.out.println("No hay repartidores disponibles en este momento.");
+            }
+        } else if (tipoComercio.equalsIgnoreCase("supermercado") && (tiempoLLegada <= 30)) {
+            if (disponibilidadInmediata) {
+                System.out.println("Repartidor asignado " + nombreRepartidor);
+                System.out.println("Tu repartidor esta a: " + getTiempoLLegada() + " minutos.");
+            } else {
+                System.out.println("No hay repartidores disponibles en este momento.");
+            }
+        }else{
+            System.out.println("El pedido no cumple con las condiciones del servicio Express " + "\n" +
+                                "(Comercio no habilitado o distancia fuera del limite de tiempo para entrega inmediata)");
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Tipo de Comercio: " + tipoComercio + "\n" +
+                "Tiempo de Llegada: " + tiempoLLegada + "\n" +
+                "¿Disponibilidad Inmediata?: " + disponibilidadInmediata;
+    }
+}
